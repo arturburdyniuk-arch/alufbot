f8e63c4577e262e0ca827b528bfe01f2704d6923
arturburdyniuk-arch <arturburdyniuk@gmail.com>
2026-05-15 20:31:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -51,9 +51,30 @@ bikes = [
     {
         "id": 1,
         "name": "DUOTTS S26",
-        "price": "1760 Kč / week",
-        "description": "Power e-bike",
+        "price": "1760+ Kč / неделя",
+        "description": "Мощный универсальный электровелосипед",
         "photo": "photos/bike1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "DUOTTS C29",
+        "price": "1440+ Kč / неделя",
+        "description": "Комфортный велосипед для города",
+        "photo": "photos/bike2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "DUOTTS C29K 2 Battery",
+        "price": "1490+ Kč / неделя",
+        "description": "Комфортный велосипед для города с двумя батареями на борту",
+        "photo": "photos/bike3.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Onesport ot08pro",
+        "price": "1600+ Kč / неделя",
+        "description": "Премиум модель с широкими колесами и двумя батареями на борту",
+        "photo": "photos/bike4.jpg"
     }
 ]
 
